415f27530b4ea79b9ab2f9fb003cd1f61771b134
islaylover <cinema.taro.ex@gmail.com>
2018-04-30 08:43:40 +0000

    delete bad code

--- app/admin/pictures.rb
+++ app/admin/pictures.rb
@@ -1,11 +1,5 @@
 ActiveAdmin.register Picture do
 
-  controller do
-    def scoped_collection
-      Question.includes(:impressions)
-    end
-  end
-
   # 許可するパラメタ指定
-  #permit_params :impression_id, :title, :title_en, :impression_en, :memo, :memo_en, :image, :delete_flg
+  permit_params :impression_id, :title, :title_en, :impression_en, :memo, :memo_en, :image, :delete_flg
 end
